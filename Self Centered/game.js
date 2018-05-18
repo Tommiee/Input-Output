6f49a50182811dad2bebd86d2aba91e39805f868
Tommiee <tomdepom1999@gmail.com>
2018-05-18 10:18:10 +0200

    fiddled about with the datareader, game itself and deactived the bomb that was inputhandler.js

--- Self Centered/game.js
+++ Self Centered/game.js
@@ -1,3 +1,6 @@
+//TODO get DataReader.js to gather Meditation info
+//TODO
+
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let canvasW, canvasH;
@@ -8,22 +11,23 @@ var withinArea;
 
 var player = new GameObject(
   new Point(0,0,20,"lightgreen"),
-  new Vector2(100,100),
-  new Vector2(1,-3),
+  new Vector2(canvasW/2,canvasH/4),
+  new Vector2(5,0),
   new Vector2(0,0)
 );
 
 var targetCircle = new Point(canvasW/2,canvasH/2,100,"green","",true,5);
-
+/*
     //spacebar placeholder "meditation above threshold"
     if(!onInside(player,targetCircle)){
       player.vel.difVector(player.pos,new Vector2(targetCircle.x,targetCircle.y));
       player.vel.r = 1;
     } else {
-      //player.vel = new Vector2(0,0);
+      player.vel = new Vector2(0,0);
     }
     //placeholder "meditation below threshold"
     player.vel.r = -1;
+*/
 
 function loop(){
   requestAnimationFrame(loop);
@@ -33,6 +37,7 @@ function loop(){
     targetCircle.y = canvasH/2;
   }
   context.clearRect(0,0,canvasW,canvasH);
+  rotateAround(player,targetCircle);
   player.draw();
   targetCircle.draw();
   player.update();
@@ -49,6 +54,12 @@ function onInside(itself,target){
   if(itself.point.distance(target) <= itself.point.r+target.r){
     return true;
   }else return false;
+}
+
+function rotateAround(self,target){
+  self.vel.difVector(self.pos,new Vector2(target.x,target.y));
+  self.vel.add(self.vel.difVector(self,new Vector2(target.x,-target.y)));
+  self.vel.r = 1;
 }
 
 loop();
